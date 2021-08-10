47a31d54e00885d82041f14909809ae70aafac3c
José Marcos Vinícius Andrade Gois <marcosvagois@outlook.com>
2021-08-10 16:43:27 -0300

    Adicionando ESLint, Prettier e Styled ao Projeto

--- src/features/restaurants/components/restaurant-info-card.js
+++ src/features/restaurants/components/restaurant-info-card.js
@@ -1,22 +1,33 @@
-import { Lato_400Regular } from "@expo-google-fonts/lato";
-import { smiley } from "cli-spinners";
-import { rest } from "lodash";
 import React from "react";
 import { Text, StyleSheet, View } from "react-native";
-import { Avatar, Button, Card, Title, Paragraph } from "react-native-paper";
+import { Card } from "react-native-paper";
 import { SvgXml } from "react-native-svg";
+import styled from "styled-components/native";
 
 import star from "../../../../assets/star";
 import open from "../../../../assets/open";
-import { colors } from "../../../infrastructure/theme/colors";
 import { fonts, fontSizes } from "../../../infrastructure/theme/fonts";
 import { space } from "../../../infrastructure/theme/spacing";
+
+const RestaurantCard = styled(Card)`
+  background-color: white;
+`;
+
+const RestaurantCardCover = styled(Card.Cover)`
+  padding: 20px;
+  background-color: white;
+`;
+
+const Title = styled(Text)`
+  padding: 16px;
+  color: red;
+`;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Some Restaurant",
-    icon,
-    photos,
+    //icon,
+    //photos,
     address = "CSB 10",
     isOpenNow = true,
     rating = 5,
@@ -27,56 +38,25 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
 
   return (
     <>
-      <Card elevation={5} style={styles.card}>
-        <Card.Cover
-          style={styles.cover}
-          source={{ uri: "https://picsum.photos/700" }}
-        />
-        <Title style={styles.title}>{name}</Title>
-        <View style={styles.rowStar}>
+      <RestaurantCard elevation={5}>
+        <RestaurantCardCover source={{ uri: "https://picsum.photos/700" }} />
+        <Title>{name}</Title>
+        {/* <Title style={styles.title}>{name}</Title> */}
+        <View>
           {ratingArray.map(() => (
             <SvgXml xml={star} width={20} height={20} />
           ))}
-          <View style={styles.open}>
+          <View>
             {isClosedTemporarily && (
-              <Text variant="label" style={{ color: "red" }}>
-                Closed Temporarily
-              </Text>
+              <Text variant="label">Closed Temporarily</Text>
             )}
-            <View style={{ marginLeft: 20 }}>
+            <View>
               {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
             </View>
           </View>
         </View>
-        <Text style={styles.address}>{address}</Text>
-      </Card>
+        <Text>{address}</Text>
+      </RestaurantCard>
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  card: {
-    padding: space.md,
-  },
-  cover: {
-    backgroundColor: "white",
-    padding: space.sm,
-  },
-  title: {
-    fontFamily: fonts.body,
-    fontSize: fontSizes.body,
-  },
-  address: {
-    fontSize: fontSizes.caption,
-    fontFamily: fonts.monospace,
-  },
-  rowStar: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  open: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "flex-end",
-  },
-});

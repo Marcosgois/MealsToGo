b4f113e78e7bc0a6554af16e44467a907b57abd7
Marcos Gois <marcosvagois@outlook.com>
2021-08-10 10:24:50 -0300

    Adicionando SVGs e importando o suporte pra elas

--- src/features/restaurants/components/restaurant-info-card.js
+++ src/features/restaurants/components/restaurant-info-card.js
@@ -1,9 +1,15 @@
 import { Lato_400Regular } from "@expo-google-fonts/lato";
+import { smiley } from "cli-spinners";
 import { rest } from "lodash";
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View } from "react-native";
 import { Avatar, Button, Card, Title, Paragraph } from "react-native-paper";
+import { SvgXml } from "react-native-svg";
+
+import star from "../../../../assets/star";
+import open from "../../../../assets/open";
 import { colors } from "../../../infrastructure/theme/colors";
+import { fonts, fontSizes } from "../../../infrastructure/theme/fonts";
 import { space } from "../../../infrastructure/theme/spacing";
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
@@ -14,19 +20,35 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
     address = "CSB 10",
     isOpenNow = true,
     rating = 5,
-    isClosedTemporarily,
+    isClosedTemporarily = true,
   } = restaurant;
+
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
 
   return (
     <>
       <Card elevation={5} style={styles.card}>
-        <Card.Content>
-          <Title>{name}</Title>
-        </Card.Content>
         <Card.Cover
           style={styles.cover}
           source={{ uri: "https://picsum.photos/700" }}
         />
+        <Title style={styles.title}>{name}</Title>
+        <View style={styles.rowStar}>
+          {ratingArray.map(() => (
+            <SvgXml xml={star} width={20} height={20} />
+          ))}
+          <View style={styles.open}>
+            {isClosedTemporarily && (
+              <Text variant="label" style={{ color: "red" }}>
+                Closed Temporarily
+              </Text>
+            )}
+            <View style={{ marginLeft: 20 }}>
+              {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            </View>
+          </View>
+        </View>
+        <Text style={styles.address}>{address}</Text>
       </Card>
     </>
   );
@@ -34,14 +56,27 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
 
 const styles = StyleSheet.create({
   card: {
-    backgroundColor: colors.bg.secondary,
-    fontFamily: Lato_400Regular,
+    padding: space.md,
   },
   cover: {
-    padding: space.md,
-    backgroundColor: colors.bg.secondary,
+    backgroundColor: "white",
+    padding: space.sm,
   },
   title: {
-    padding: space.md,
+    fontFamily: fonts.body,
+    fontSize: fontSizes.body,
+  },
+  address: {
+    fontSize: fontSizes.caption,
+    fontFamily: fonts.monospace,
+  },
+  rowStar: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  open: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "flex-end",
   },
 });

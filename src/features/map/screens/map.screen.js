b5fa6710e26263e2ec97077ade8f8c52a1898de4
José Marcos Vinícius Andrade Gois <marcosvagois@outlook.com>
2021-08-22 17:10:53 -0300

    Adding Map and SearchBar

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,6 +1,15 @@
 import React, { useContext, useState, useEffect } from "react";
+import { Search } from "../components/search.component";
 import MapView from "react-native-maps";
+import styled from "styled-components";
 
-export const MapScreen = () => {
-  return <MapView />;
-};
+const Map = styled(MapView)`
+  height: 100%;
+  width: 100%;
+`;
+export const MapScreen = () => (
+  <>
+    <Search />
+    <Map />
+  </>
+);

0eba8a9f6ffebd5b6507223a66a5b75ad2d823e7
José Marcos Vinícius Andrade Gois <marcosvagois@outlook.com>
2021-09-21 16:04:00 -0300

    Adding the watermelon moving

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,7 +1,7 @@
 import React from "react";
+import LottieView from "lottie-react-native";
 
 import { Spacer } from "../../../components/spacer/spacer.component";
-import { Navigation } from "../../../infrastructure/navigation";
 
 import {
   AccountBackground,
@@ -9,12 +9,22 @@ import {
   AccountCover,
   AuthButton,
   Title,
+  AnimationWrapper,
 } from "../components/account.styles";
 
 export const AccountScreen = ({ navigation }) => {
   return (
     <AccountBackground>
       <AccountCover />
+      <AnimationWrapper>
+        <LottieView
+          key="animation"
+          autoPlay
+          loop
+          resizeMode="cover"
+          source={require("../../../../assets/watermelon.json")}
+        />
+      </AnimationWrapper>
       <Title>Meals To Go</Title>
       <AccountContainer>
         <Spacer position="bottom" size="medium">

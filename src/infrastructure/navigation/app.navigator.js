c83806b04287767150939a8ccef6164725390e90
José Marcos Vinícius Andrade Gois <goisinho_5@hotmail.com>
2022-02-01 02:29:44 -0300

    Resolvendo Warnings TabBarOptions e NestedScreens

--- src/infrastructure/navigation/app.navigator.js
+++ src/infrastructure/navigation/app.navigator.js
@@ -13,8 +13,8 @@ import { FavouritesContextProvider } from "../../services/favourites/favourites.
 const Tab = createBottomTabNavigator();
 
 const TAB_ICON = {
-  Restaurants: "md-restaurant",
-  Map: "md-map",
+  Restaurantes: "md-restaurant",
+  Mapa: "md-map",
   Settings: "md-settings",
 };
 
@@ -24,6 +24,8 @@ const createScreenOptions = ({ route }) => {
     tabBarIcon: ({ size, color }) => (
       <Ionicons name={iconName} size={size} color={color} />
     ),
+    tabBarActiveTintColor: "tomato",
+    inactiveTintColor: "gray",
   };
 };
 
@@ -33,13 +35,9 @@ export const AppNavigator = () => (
       <RestaurantsContextProvider>
         <Tab.Navigator
           screenOptions={createScreenOptions}
-          tabBarOptions={{
-            tabBarActiveTintColor: "tomato",
-            inactiveTintColor: "gray",
-          }}
         >
-          <Tab.Screen name="Restaurants" component={RestaurantsNavigator} />
-          <Tab.Screen name="Map" component={MapScreen} />
+          <Tab.Screen name="Restaurantes" component={RestaurantsNavigator} />
+          <Tab.Screen name="Mapa" component={MapScreen} />
           <Tab.Screen name="Settings" component={SettingsNavigator} />
         </Tab.Navigator>
       </RestaurantsContextProvider>

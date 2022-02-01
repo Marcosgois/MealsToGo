b9b24bee4e5add7b4fd9f93df9bf566271f5e81e
José Marcos Vinícius Andrade Gois <goisinho_5@hotmail.com>
2022-02-01 05:36:46 -0300

    Implementando Câmera, salvar e mostrar imagem

--- src/features/settings/screens/settings.screen.js
+++ src/features/settings/screens/settings.screen.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import styled from "styled-components/native";
 
 import { List, Avatar } from "react-native-paper";
@@ -7,6 +7,9 @@ import { Text } from " ../../../components/typography/text.component";
 import { Spacer } from " ../../../components/spacer/spacer.component";
 import { SafeArea } from " ../../../components/utility/safe-area.component";
 import { AuthenticationContext } from "../../../services/authentication/authentication.context";
+import { TouchableOpacity } from "react-native";
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const SettingsItem = styled(List.Item)`
     padding: ${props => props.theme.space[3]}
@@ -18,11 +21,31 @@ const AvatarContainer = styled.View`
 
 export const SettingsScreen = ({ navigation }) => {
     const { onLogout, user } = useContext(AuthenticationContext);
+    const [photo, setPhoto] = useState(null);
+
+    const getProfilePicture = async (currentUser) => {
+        const photoUri = await AsyncStorage.getItem(`${currentUser.uid}-photo`);
+        setPhoto(photoUri);
+    }
+
+    useEffect(() => {
+        getProfilePicture(user);
+    }, [user])
+
     return (
         <SafeArea>
             <AvatarContainer>
                 <Spacer position="top" size="large">
-                    <Avatar.Icon size={180} icon="human" backgroundColor="#2182BD" />
+                    <TouchableOpacity onPress={() => navigation.navigate("Camera")}>
+                        {!photo && <Avatar.Icon
+                            size={180} icon="human"
+                            backgroundColor="#2182BD" />}
+                        {photo && <Avatar.Image
+                            size={180}
+                            source={{ uri: photo }}
+                            backgroundColor="#2182BD" />}
+
+                    </TouchableOpacity>
                 </Spacer>
                 <Spacer position="top" size="large">
                     <Text variant="caption">{user.email}</Text>
--- src/infrastructure/navigation/settings.navigator.js
+++ src/infrastructure/navigation/settings.navigator.js
@@ -2,6 +2,7 @@ import React from "react";
 import { SettingsScreen } from "../../features/settings/screens/settings.screen";
 import { FavouritesScreen } from "../../features/settings/screens/favourites.screen";
 import { ProfileScreen } from "../../features/settings/screens/profile.screen";
+import { CameraScreen } from "../../features/settings/screens/camera.screen";
 
 import {
     createStackNavigator,
@@ -27,16 +28,22 @@ export const SettingsNavigator = ({ route, navigation }) => {
             />
             <SettingsStack.Screen
                 options={{
-                    header: () => null,
+                    //header: () => null,
                 }}
                 name="Favourites"
                 component={FavouritesScreen} />
             <SettingsStack.Screen
                 options={{
-                    header: () => null,
+                    //header: () => null,
                 }}
                 name="Profile"
                 component={ProfileScreen} />
+            <SettingsStack.Screen
+                options={{
+                    //header: () => null,
+                }}
+                name="Camera"
+                component={CameraScreen} />
         </SettingsStack.Navigator>
     );
 }
